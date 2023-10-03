9329ce70d5e54cb242d2f088ddd0fa66a0faaf11
Lior Noy <lnoy@redhat.com>
2023-10-03 12:20:08 +0300

    Create bash script to fetch cluster ports
    This commit creates the tests/runSSonNodes.sh script
    to fetch the open ports on the cluster.
    
    We create custom endpointSlices from the tests/customEndpointSlices.json
    file.
    
    Signed-off-by: Lior Noy <lnoy@redhat.com>

--- tests/nodeComm_test.go
+++ tests/nodeComm_test.go
@@ -3,45 +3,68 @@ package nodecommlib
 import (
 	"bufio"
 	"context"
+	"encoding/json"
 	"fmt"
 	"os"
+	"os/exec"
+	"strconv"
 	"strings"
 
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
 
-	_ "github.com/openshift/cluster-node-tuning-operator/test/e2e/performanceprofile/functests/utils/client" // This is to init the nto client
-	"github.com/openshift/cluster-node-tuning-operator/test/e2e/performanceprofile/functests/utils/nodes"
+	corev1 "k8s.io/api/core/v1"
 	discoveryv1 "k8s.io/api/discovery/v1"
+	"k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 
 	"github.com/liornoy/main/node-comm-lib/pkg/client"
 	"github.com/liornoy/main/node-comm-lib/pkg/commatrix"
 	"github.com/liornoy/main/node-comm-lib/pkg/endpointslices"
+	"github.com/liornoy/main/node-comm-lib/pkg/pointer"
 )
 
-type TheOuput struct {
-	State        string `json:"State"`
-	RecvQ        string `json:"Recv-Q"`
-	SendQ        string `json:"Send-Q"`
-	AddrPort     string `json:"Local Address:Port"`
-	PeerAddrPort string `json:"Peer Address:Port"`
-	Process      string `json:"Process"`
-}
+var (
+	cs  *client.ClientSet
+	err error
+)
 
 var _ = Describe("Comm Matrix", func() {
+	BeforeEach(func() {
+		cs, err = client.New("")
+		Expect(err).ToNot(HaveOccurred())
+
+		By("generating custom EndpointSlices for host services")
+		err = createHostServiceSlices(cs)
+		Expect(err).ToNot(HaveOccurred())
+
+		By("fetching all ports cluster is listening to")
+		_, err = exec.Command("./runSSonNodes.sh").Output()
+		Expect(err).ToNot(HaveOccurred())
+	})
+
+	AfterEach(func() {
+		By("fetching all custom EndpointSlices and deleting them")
+		customeSlices, err := cs.EndpointSlices("default").List(context.TODO(), metav1.ListOptions{})
+		Expect(err).ToNot(HaveOccurred())
+
+		for _, slice := range customeSlices.Items {
+			if !strings.Contains(slice.Name, "test") {
+				continue
+			}
+			err := cs.EndpointSlices("default").Delete(context.TODO(), slice.Name, metav1.DeleteOptions{})
+			Expect(err).ToNot(HaveOccurred())
+		}
+	})
+
 	Context("create a comm matrix from the cluster", func() {
 		It("should equal to what the nodes are actually listening on", func() {
-			stdout := os.Stdout
-
-			cs, err := client.New("")
-			Expect(err).ToNot(HaveOccurred())
-
 			clusterComMat, err := generateClusterComMatrix(cs)
 			Expect(err).ToNot(HaveOccurred())
 
-			outfile, err := os.Create("./artifacts/clusterComMat.txt")
+			outfile, err := os.Create("./artifacts/ss-command-com-matrix.txt")
 			Expect(err).ToNot(HaveOccurred())
+			stdout := os.Stdout
 			os.Stdout = outfile
 			clusterComMat.PrintMat()
 			outfile.Close()
@@ -49,14 +72,10 @@ var _ = Describe("Comm Matrix", func() {
 			slices, err := endpointslices.GetIngressCommSlices(cs)
 			Expect(err).ToNot(HaveOccurred())
 
-			if len(slices) == 0 {
-				fmt.Println("GetIngressCommSlices returned no slices!")
-				return
-			}
 			endpointSliceMat, err := commatrix.CreateCommMatrix(cs, slices)
 			Expect(err).ToNot(HaveOccurred())
 
-			outfile, err = os.Create("./artifacts/endpointSlicesComMan.txt")
+			outfile, err = os.Create("./artifacts/endpointslices-com-matirx.txt")
 			Expect(err).ToNot(HaveOccurred())
 			os.Stdout = outfile
 			endpointSliceMat.PrintMat()
@@ -67,47 +86,107 @@ var _ = Describe("Comm Matrix", func() {
 	})
 })
 
+func createHostServiceSlices(cs *client.ClientSet) error {
+	nodes, err := cs.Nodes().List(context.TODO(), metav1.ListOptions{})
+	if err != nil {
+		return err
+	}
+
+	nodesRoles := commatrix.GetNodesRoles(nodes)
+	rolesNodes := reverseMap(nodesRoles)
+
+	slices, err := customHostServicesDefinition()
+	if err != nil {
+		return err
+	}
+
+	for _, s := range slices {
+		port, err := strconv.ParseInt(s.Port, 10, 32)
+		if err != nil {
+			return err
+		}
+		name := fmt.Sprintf("test-%s-%s-%s", s.ServiceName, s.NodeRole, s.Port)
+		name = strings.ToLower(name)
+
+		nodeName := rolesNodes[s.NodeRole]
+
+		endpointSlice := discoveryv1.EndpointSlice{
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      name,
+				Namespace: "default",
+				Labels:    map[string]string{"ingress": "", "kubernetes.io/service-name": s.ServiceName},
+			},
+			Ports: []discoveryv1.EndpointPort{
+				{
+					Port:     pointer.Int32Ptr(int32(port)),
+					Protocol: (*corev1.Protocol)(&s.Protocol),
+				},
+			},
+			Endpoints: []discoveryv1.Endpoint{
+				{
+					NodeName:  pointer.StrPtr(nodeName),
+					Addresses: []string{"1.1.1.1"},
+				},
+			},
+			AddressType: "IPv4",
+		}
+		_, err = cs.EndpointSlices("default").Create(context.TODO(), &endpointSlice, metav1.CreateOptions{})
+		if err != nil && !errors.IsAlreadyExists(err) {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func reverseMap(m map[string]string) map[string]string {
+	n := make(map[string]string, len(m))
+	for k, v := range m {
+		n[v] = k
+	}
+	return n
+}
+
+func customHostServicesDefinition() ([]commatrix.CommDetails, error) {
+	var res []commatrix.CommDetails
+	bs, err := os.ReadFile("customEndpointSlices.json")
+	if err != nil {
+		return nil, err
+	}
+
+	err = json.Unmarshal(bs, &res)
+	if err != nil {
+		return nil, err
+	}
+
+	return res, nil
+}
+
 func generateClusterComMatrix(cs *client.ClientSet) (commatrix.CommMatrix, error) {
 	var res = commatrix.CommMatrix{}
 
-	// Get open ports from nodes and create Comm Matrix to compare
-	dnodes, err := cs.Nodes().List(context.TODO(), metav1.ListOptions{})
+	nodes, err := cs.Nodes().List(context.TODO(), metav1.ListOptions{})
 	Expect(err).ToNot(HaveOccurred())
 
-	nodesRoles, err := commatrix.GetNodesRoles(cs)
-	Expect(err).ToNot(HaveOccurred())
+	nodesRoles := commatrix.GetNodesRoles(nodes)
 
-	outPuts := make([]commatrix.CommDetails, 0)
-	for _, n := range dnodes.Items {
-		tcpOutput, err := nodes.ExecCommandOnNode([]string{"ss", "-plant"}, &n)
+	comDetails := make([]commatrix.CommDetails, 0)
+	for _, n := range nodes.Items {
+		tcpOutput, err := os.ReadFile("./artifacts/" + n.Name + "-tcp.txt")
 		Expect(err).ToNot(HaveOccurred())
 
-		//Debug
-		outfile, err := os.Create("./artifacts/" + n.Name + "-tcp.txt")
-		Expect(err).ToNot(HaveOccurred())
-		outfile.WriteString(tcpOutput)
-		outfile.Close()
-		//
+		tcpComDetails := ssToCommDetails(string(tcpOutput), nodesRoles[n.Name], "TCP")
+		comDetails = append(comDetails, tcpComDetails...)
 
-		tcpComDetails := ssToCommDetails(tcpOutput, nodesRoles[n.Name], "TCP")
-		outPuts = append(outPuts, tcpComDetails...)
-
-		udpOutput, err := nodes.ExecCommandOnNode([]string{"ss", "-planu"}, &n)
+		udpOutput, err := os.ReadFile("./artifacts/" + n.Name + "-udp.txt")
 		Expect(err).ToNot(HaveOccurred())
 
-		//Debug
-		outfile, err = os.Create("./artifacts/" + n.Name + "-udp.txt")
-		Expect(err).ToNot(HaveOccurred())
-		outfile.WriteString(udpOutput)
-		outfile.Close()
-		//
-
-		udpComDetails := ssToCommDetails(udpOutput, nodesRoles[n.Name], "UDP")
-		outPuts = append(outPuts, udpComDetails...)
+		udpComDetails := ssToCommDetails(string(udpOutput), nodesRoles[n.Name], "UDP")
+		comDetails = append(comDetails, udpComDetails...)
 	}
 
-	outPuts = commatrix.RemoveDups(outPuts)
-	res.Matrix = outPuts
+	comDetails = commatrix.RemoveDups(comDetails)
+	res.Matrix = comDetails
 
 	return res, nil
 }
