ac01acdd2ea5960595c1bd1705a4630d4fd06b59
Lior Noy <lnoy@redhat.com>
2023-10-22 20:03:58 +0300

    e2etest: change flags order in runSSonNodes.sh
    We modify the order of the flags in the "ss" command
    and move the "a" flag to the start in order to get better
    results, this change is required because how the flags
    in "ss" are implemented.
    
    Signed-off-by: Lior Noy <lnoy@redhat.com>

--- e2etest/hack/runSSonNodes.sh
+++ e2etest/hack/runSSonNodes.sh
@@ -7,6 +7,6 @@ PODS=$(oc get pods -n openshift-machine-config-operator -o name | grep machine-c
 
 for POD in ${PODS}; do
     NODE=$(oc get "${POD}" -n openshift-machine-config-operator -o 'jsonpath={.spec.nodeName}')
-    oc exec "${POD}" -n openshift-machine-config-operator -c machine-config-daemon -- ss -plant > $ARTIFACTS/"${NODE}"-tcp.txt
-    oc exec "${POD}" -n openshift-machine-config-operator -c machine-config-daemon -- ss -planu > $ARTIFACTS/"${NODE}"-udp.txt
+    oc exec "${POD}" -n openshift-machine-config-operator -c machine-config-daemon -- ss -anplt > $ARTIFACTS/"${NODE}"-tcp.txt
+    oc exec "${POD}" -n openshift-machine-config-operator -c machine-config-daemon -- ss -anplu > $ARTIFACTS/"${NODE}"-udp.txt
 done
