d9c65e851e56a484f8fd37dde758b85aeb839082
Lior Noy <lnoy@redhat.com>
2023-11-06 20:49:12 +0200

    Add new "nftables" module
    The new 'nftables' module is responsible for creating
    the nftables rules that block all ports except for the
    ones in the communication matrix.
    
    The function for this task is 'GetRulesFromCommDetails.'
    It only returns an nftable file (or an error), and the
    caller is responsible for executing the update on the node.
    
    Signed-off-by: Lior Noy <lnoy@redhat.com>

---
+++ pkg/nftables/nftables.go
@@ -0,0 +1,61 @@
+package nftables
+
+import (
+	"bytes"
+	"html/template"
+
+	"github.com/liornoy/node-comm-lib/pkg/commatrix"
+)
+
+type NftablesData struct {
+	AllowedTCPPorts []string
+	AllowedUDPPorts []string
+}
+
+const nftablesTemplate = `#!/usr/sbin/nft -f
+
+table ip my_filter {
+    chain input {
+        type filter hook input priority 0; policy drop;
+
+        iifname "lo" accept;
+
+        # Hard-coded rule to allow SSH traffic for safety
+        tcp dport 22 accept;
+		
+        tcp dport { {{range .AllowedTCPPorts}}{{.}}, {{end}} } accept;
+        udp dport { {{range .AllowedUDPPorts}}{{.}}, {{end}} } accept;
+    }
+}
+`
+
+func GetRulesFromCommDetails(cds []commatrix.ComDetails) (string, error) {
+	var (
+		nftablesContent bytes.Buffer
+		data            = NftablesData{
+			AllowedTCPPorts: make([]string, 0),
+			AllowedUDPPorts: make([]string, 0),
+		}
+	)
+
+	for _, cd := range cds {
+		if cd.Protocol == "TCP" {
+			data.AllowedTCPPorts = append(data.AllowedTCPPorts, cd.Port)
+		}
+		if cd.Protocol == "UDP" {
+			data.AllowedUDPPorts = append(data.AllowedUDPPorts, cd.Port)
+		}
+	}
+
+	tmpl, err := template.New("nftablesTemplate").Parse(nftablesTemplate)
+	if err != nil {
+		return "", err
+	}
+
+	err = tmpl.Execute(&nftablesContent, data)
+	if err != nil {
+		return "", err
+	}
+
+	return nftablesContent.String(), nil
+}
